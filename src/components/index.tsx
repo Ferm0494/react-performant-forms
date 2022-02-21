c514e57b01301731edbe26070657f2972eb73bef
Ferm0494 <ferm0494@gmail.com>
2022-02-20 22:54:32 -0600

    First commit

---
+++ src/components/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface IDebouncedInputProps extends Omit<React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>, 'value'> {}
+
+/* With debounce we dont use value prop , we store it until the end  */
+export const DebouncedTextInput = (props: IDebouncedInputProps) => {
+    return <input type="text" {...props} />;
+};
+/* We are not using debounce we can store our state value into the value prop  */
+export const NonDebouncedTextInput = (props:React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> ) => {
+    return <input type="text" {...props} />;
+};
+
