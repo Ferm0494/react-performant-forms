ff24a79564acbe16b1e7e99b928cbdd4de1443cf
ferm0494 <ferm0494@gmail.com>
2022-05-21 16:29:37 -0600

    Update export

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import useForm  from './hooks/useForm'
 import usePaginatedScroll from './hooks/usePaginatedScroll'
+import useResponsive from './hooks/useResponsive'
 export {
   useForm,
-  usePaginatedScroll
+  usePaginatedScroll,
+  useResponsive
 }
