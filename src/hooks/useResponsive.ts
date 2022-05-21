73d21d77d278d677425ac8f86811f8e57da9bfdb
ferm0494 <ferm0494@gmail.com>
2022-05-21 14:58:17 -0600

    useResponsive within hook core

--- src/hooks/useResponsive.ts
+++ src/hooks/useResponsive.ts
@@ -1,22 +1,11 @@
 import {useState,useEffect} from 'react'
-
-interface BreakingPoints{
-    xs?:number,
-    sm?: number,
-    md?: number,
-    lg?:number,
-    xl?:number,
-
-}
-
-const useResponsive = ({
-    sm=576,
-    md=768,
-    lg=992,
-    xl=1200
-
-}: BreakingPoints)=>{
-
+/** 
+* Keep screen dimensions within one hook
+* @param {boolean} resizeListener - If true evertime useResponsive its going to be measuring the screen width, to set dimension values, Default value is *true*
+* @param {breakingPoints} breakingPoints - NOT RECOMMENDED: But you can set your own limits for each dimension.
+*/
+const useResponsive = (resizeListener= true, breakingPoints = { sm :576, md:768, lg:992, xl:1200 })=>{
+    const {sm,md,lg,xl} = breakingPoints;
     const [width,setWidth]= useState({
         xs: window.innerWidth < sm,
         sm: window.innerWidth < md && window.innerWidth >= sm,
@@ -27,6 +16,7 @@ const useResponsive = ({
 
     useEffect(()=>{
            const handleResize = ()=>{
+               console.log('Resizing!')
             setWidth({
                 xs: window.innerWidth < sm,
                 sm: window.innerWidth < md && window.innerWidth >= sm,
@@ -35,10 +25,14 @@ const useResponsive = ({
                 xl: window.innerWidth >= xl       
             });
            }
-
-           window.addEventListener("resize",handleResize);
-           handleResize();
-           return ()=> window.removeEventListener("resize",handleResize);
+           if(resizeListener){
+            window.addEventListener("resize",handleResize);
+           }
+           return ()=> {
+               if(resizeListener){
+                return window.removeEventListener("resize",handleResize)
+               }
+           };
     },[])
 
     return{
